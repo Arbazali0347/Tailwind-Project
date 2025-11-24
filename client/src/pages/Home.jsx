a8859622c1656f934371133c85271c4bcd63abea
Arbazali0347 <arbazali.coder2025@gmail.com>
2025-11-24 21:51:25 +0500

    Remove the sec2 line

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import React, { useContext } from 'react'
 import { AppContextProvider } from '../context/AppContext'
 import Section1 from '../components/section1/Section1'
-import Section2 from '../components/section2/section2'
 
 
 const Home = () => {
